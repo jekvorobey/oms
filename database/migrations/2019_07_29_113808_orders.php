2b51833a91854c83a0353a7a938c5418fbee4233
Yury Kudryavtsev <kudryavtsev@greensight.ru>
2019-09-05 16:25:09 +0300

    #48657 Orders history migration

--- database/migrations/2019_07_29_113808_orders.php
+++ database/migrations/2019_07_29_113808_orders.php
@@ -16,7 +16,7 @@ class Orders extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('customer_id')->unsigned()->nullable();
-        
+
             $table->string('number');
             $table->decimal('cost', 18, 4);
             $table->tinyInteger('status')->unsigned()->default(1);
@@ -26,34 +26,50 @@ class Orders extends Migration
             $table->dateTime('processing_time');
             $table->dateTime('delivery_time');
             $table->text('comment')->nullable();
-        
+
             $table->timestamps();
         });
-        
+
+        Schema::create('orders_history', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->bigInteger('order_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->tinyInteger('type')->unsigned()->nullable();
+            $table->jsonb('data')->nullable();
+
+            $table->bigInteger('entity')->unsigned()->nullable();
+            $table->bigInteger('entity_id')->unsigned()->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders');
+        });
+
         Schema::create('baskets', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('customer_id')->unsigned()->nullable();
             $table->bigInteger('order_id')->unsigned()->nullable();
-        
+
             $table->timestamps();
-    
+
             $table->foreign('order_id')->references('id')->on('orders');
         });
-        
+
         Schema::create('basket_items', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('basket_id')->unsigned()->nullable();
             $table->bigInteger('offer_id')->unsigned()->nullable();
-    
+
             $table->string('name');
             $table->decimal('qty', 18, 4);
             $table->decimal('price', 18, 4);
             $table->boolean('is_reserved')->default(false);
             $table->bigInteger('reserved_by')->nullable();
             $table->timestamp('reserved_at')->nullable();
-        
+
             $table->timestamps();
-    
+
             $table->foreign('basket_id')->references('id')->on('baskets');
         });
     }
@@ -68,5 +84,6 @@ class Orders extends Migration
         Schema::dropIfExists('basket_items');
         Schema::dropIfExists('baskets');
         Schema::dropIfExists('orders');
+        Schema::dropIfExists('orders_history');
     }
 }
