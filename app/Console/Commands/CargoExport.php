86b20b94fff99fd000a18169776d7698d8c86892
Evgeny Bereza <bereza@greensight.ru>
2020-01-13 19:00:41 +0300

    Готова консольная команда для создания заявок на вызов курьера

--- app/Console/Commands/CargoExport.php
+++ app/Console/Commands/CargoExport.php
@@ -7,14 +7,17 @@ use App\Models\Delivery\CargoStatus;
 use Greensight\Logistics\Dto\CourierCall\CourierCallInput\CourierCallInputDto;
 use Greensight\Logistics\Dto\CourierCall\CourierCallInput\DeliveryCargoDto;
 use Greensight\Logistics\Dto\CourierCall\CourierCallInput\SenderDto;
-use Greensight\Logistics\Dto\Lists\CourierCallTime\B2CplCourierCallTime;
-use Greensight\Logistics\Dto\Lists\DeliveryService;
 use Greensight\Logistics\Services\CourierCallService\CourierCallService;
 use Greensight\Store\Dto\StorePickupTimeDto;
 use Greensight\Store\Services\StoreService\StoreService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Collection;
+use MerchantManagement\Services\MerchantService\MerchantService;
 
+/**
+ * Class CargoExport
+ * @package App\Console\Commands
+ */
 class CargoExport extends Command
 {
     /**
@@ -33,8 +36,7 @@ class CargoExport extends Command
 
     /**
      * Execute the console command.
-     *
-     * @return mixed
+     * @throws \Exception
      */
     public function handle()
     {
@@ -43,7 +45,6 @@ class CargoExport extends Command
             ->where('status', CargoStatus::STATUS_CREATED)
             ->where('store_id', $this->argument('storeId'))
             ->where('delivery_service', $this->argument('deliveryService'))
-            ->groupBy('delivery_service')
             ->whereHas('shipments')
             ->with('shipments.delivery')
             ->orderBy('created_at')
@@ -52,22 +53,37 @@ class CargoExport extends Command
             /** @var StoreService $storeService */
             $storeService = resolve(StoreService::class);
             $storeQuery = $storeService->newQuery()
+                ->include('storeContact')
                 ->include('storePickupTime');
             $store = $storeService->store($cargo->store_id, $storeQuery);
-            if (!is_null($store)) {
+            if (is_null($store)) {
                 //todo Добавить оповещение о невыгруженном грузе
                 return;
             }
+
+            /** @var MerchantService $merchantService */
+            $merchantService = resolve(MerchantService::class);
+            $merchant = $merchantService->merchant($store->merchant_id);
 
             $courierCallInputDto = new CourierCallInputDto();
 
             $senderDto = new SenderDto();
             $courierCallInputDto->sender = $senderDto;
-            $senderDto->address_string = join(', ', array_filter([
-                $store->zip,
-                $store->city,
-                $store->city,
-            ]));
+            $senderDto->address_string = isset($store->address['address_string']) ? $store->address['address_string'] : '';
+            $senderDto->post_index = isset($store->address['post_index']) ? $store->address['post_index'] : '';
+            $senderDto->country_code = isset($store->address['country_code']) ? $store->address['country_code'] : '';
+            $senderDto->region = isset($store->address['region']) ? $store->address['region'] : '';
+            $senderDto->area = isset($store->address['area']) ? $store->address['area'] : '';
+            $senderDto->city = isset($store->address['city']) ? $store->address['city'] : '';
+            $senderDto->city_guid = isset($store->address['city_guid']) ? $store->address['city_guid'] : '';
+            $senderDto->street = isset($store->address['street']) ? $store->address['street'] : '';
+            $senderDto->house = isset($store->address['house']) ? $store->address['house'] : '';
+            $senderDto->block = isset($store->address['block']) ? $store->address['block'] : '';
+            $senderDto->flat = isset($store->address['flat']) ? $store->address['flat'] : '';
+            $senderDto->company_name = $merchant->legal_name;
+            $senderDto->contact_name = !is_null($store->storeContact()) ? $store->storeContact()[0]->name : '';
+            $senderDto->email = !is_null($store->storeContact()) ? $store->storeContact()[0]->email : '';
+            $senderDto->phone = !is_null($store->storeContact()) ? $store->storeContact()[0]->phone : '';
 
             $deliveryCargoDto = new DeliveryCargoDto();
             $courierCallInputDto->cargo = $deliveryCargoDto;
@@ -78,6 +94,9 @@ class CargoExport extends Command
             foreach ($cargo->shipments as $shipment) {
                 $deliveryCargoDto->order_ids[] = $shipment->delivery->xml_id;
             }
+
+            /** @var CourierCallService $courierCallService */
+            $courierCallService = resolve(CourierCallService::class);
 
             //Получаем доступные дни недели для отгрузки грузов курьерам службы доставки текущего груза
             /** @var Collection|StorePickupTimeDto[] $storePickupTimes */
@@ -109,6 +128,8 @@ class CargoExport extends Command
             $date = new \DateTime();
             while ($dayPlus <= 6) {
                 $date = $date->modify('+' . $dayPlus . 'day' . ($dayPlus > 1 ?  's': ''));
+                $dayPlus++;
+
                 //Получаем номер дня недели (1 - понедельник, ..., 7 - воскресенье)
                 $dayOfWeek = $date->format('N');
                 if (!$storePickupTimes->has($dayOfWeek)) {
@@ -117,7 +138,21 @@ class CargoExport extends Command
                 $deliveryCargoDto->date = $date->format('d.m.Y');
                 $deliveryCargoDto->time = $storePickupTimes[$dayOfWeek]->pickup_time;
 
-                $dayPlus++;
+                try {
+                    $courierCallOutputDto = $courierCallService->createCourierCall(
+                        $cargo->delivery_service,
+                        $courierCallInputDto
+                    );
+                    if ($courierCallOutputDto->success) {
+                        $cargo->xml_id = $courierCallOutputDto->xml_id;
+                        $cargo->status = CargoStatus::STATUS_REQUEST_SEND;
+
+                        $cargo->save();
+                        break;
+                    }
+                } catch (\Exception $e) {
+                    dump($e->getMessage());
+                }
             }
         }
     }
