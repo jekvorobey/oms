f668ef03851e15161317ac0472957d5f941e8a77
Evgeny Bereza <bereza@greensight.ru>
2020-03-19 16:02:18 +0300

    #56151 В очередной раз поправлен пересчет суммы отправления

---
+++ app/Console/Commands/ShipmentCostRecalc.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Delivery\Shipment;
+use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+
+/**
+ * Разовая команда!
+ * Class ShipmentCostRecalc
+ * @package App\Console\Commands
+ */
+class ShipmentCostRecalc extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'shipment:cost_recalc';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Пересчёт стоимости у отправлений';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        /** @var Collection|Shipment[] $shipments */
+        $shipments = Shipment::query()->with('basketItems')->get();
+        foreach ($shipments as $shipment) {
+            $shipment->costRecalc();
+        }
+    }
+}
