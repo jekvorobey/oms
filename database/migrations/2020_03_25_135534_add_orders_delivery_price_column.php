a1a4f7c906be7f474ec3f8d82d329caa64d8b095
Kapitanov Andrey <andrey@kapdx.ru>
2020-03-25 14:17:05 +0300

    #57029 migration add orders delivery price column

---
+++ database/migrations/2020_03_25_135534_add_orders_delivery_price_column.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\Models\Order\Order;
+
+class AddOrdersDeliveryPriceColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->decimal('delivery_price', 18, 4)
+                ->default(0.0)
+                ->after('price');
+        });
+
+        Order::query()->update([
+            'delivery_price' => DB::raw("`delivery_cost`")
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('delivery_price');
+        });
+    }
+}
