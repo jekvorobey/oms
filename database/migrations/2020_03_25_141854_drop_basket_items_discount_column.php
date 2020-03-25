24c8037ed5df25133295ad7b2b1d1bf11af94d29
Kapitanov Andrey <andrey@kapdx.ru>
2020-03-25 14:25:15 +0300

    #57029 migrate drop basket items discount column

---
+++ database/migrations/2020_03_25_141854_drop_basket_items_discount_column.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\Models\Basket\BasketItem;
+
+class DropBasketItemsDiscountColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('basket_items', function (Blueprint $table) {
+            $table->dropColumn('discount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('basket_items', function (Blueprint $table) {
+            $table->decimal('discount', 18, 4)->nullable();
+        });
+
+        BasketItem::query()->update([
+            'discount' => DB::raw("`cost` - `price`")
+        ]);
+    }
+}
