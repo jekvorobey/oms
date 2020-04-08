2417768b625244d74c52c342aa01e246a7bd3187
Kapitanov Andrey <andrey@kapdx.ru>
2020-04-08 15:06:35 +0300

    OrderPromoCodes Foreign

---
+++ database/migrations/2020_04_08_150130_add_order_promo_codes_index.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddOrderPromoCodesIndex extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_promo_codes', function (Blueprint $table) {
+            $table->foreign('order_id')->references('id')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_promo_codes', function (Blueprint $table) {
+            $table->dropForeign('order_promo_codes_order_id_foreign');
+        });
+
+    }
+}
