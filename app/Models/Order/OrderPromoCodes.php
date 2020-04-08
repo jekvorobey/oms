3e150e2db40a1f41ef52e1940d878a76df5390f3
Kapitanov Andrey <andrey@kapdx.ru>
2020-04-08 09:25:03 +0300

    #55237 OrderPromoCodes migrate

---
+++ app/Models/Order/OrderPromoCodes.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models\Order;
+
+use App\Models\OmsModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+/**
+ * Class OrderPromoCodes
+ * @package App\Models\Order
+ *
+ * @property int $id
+ * @property int $order_id
+ * @property int $promo_code_id
+ * @property string $name
+ * @property string $code
+ * @property int $type
+ * @property int|null $discount_id
+ * @property int|null $gift_id
+ * @property int|null $bonus_id
+ * @property boolean $is_personal
+ */
+class OrderPromoCodes extends OmsModel
+{
+    /** @var string */
+    protected $table = 'order_promo_codes';
+
+    /**
+     * Заполняемые поля модели
+     */
+    const FILLABLE = [
+        'order_id',
+        'promo_code_id',
+        'name',
+        'code',
+        'type',
+        'discount_id',
+        'gift_id',
+        'bonus_id',
+        'is_personal',
+    ];
+
+    /** @var array */
+    protected $casts = [
+        'is_personal' => 'bool',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $fillable = self::FILLABLE;
+
+    /**
+     * @return BelongsTo
+     */
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
