093acad6fc888930bb11e4c270d01c29fbeab742
Evgeny Bereza <bereza@greensight.ru>
2020-04-29 13:44:15 +0300

    #54722 В BasketItem для товаров добавлено сохранение даты старта продаж для офферов. Часть 2

---
+++ app/Console/Commands/AddSaleAtToBasketItem.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Basket\Basket;
+use App\Models\Basket\BasketItem;
+use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+use Pim\Dto\Offer\OfferDto;
+use Pim\Services\OfferService\OfferService;
+
+/**
+ * Разовая команда! Удалить после использования на всех основных площадках (dev и master)
+ * Class AddSaleAtToBasketItem
+ * @package App\Console\Commands
+ */
+class AddSaleAtToBasketItem extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'basket_item:add_sale_at';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Добавить sale_at для элементов корзины, являющихся товарами';
+
+    /**
+     * Execute the console command.
+     * @throws \Pim\Core\PimException
+     */
+    public function handle()
+    {
+        /** @var Collection|BasketItem[] $basketItems */
+        $basketItems = BasketItem::query()
+            ->where('type', Basket::TYPE_PRODUCT)
+            ->get();
+
+        $offersIds = $basketItems->pluck('offer_id')->unique()->all();
+        /** @var Collection|OfferDto[] $offers */
+        $offers = collect();
+        foreach (array_chunk($offersIds, 50) as $chunkedOffersIds) {
+            /** @var OfferService $offerService */
+            $offerService = resolve(OfferService::class);
+            $offersQuery = $offerService->newQuery()
+                ->setFilter('id', $chunkedOffersIds)
+                ->addFields(OfferDto::entity(), 'id', 'sale_at');
+            $offers = $offers->merge($offerService->offers($offersQuery));
+        }
+        $offers = $offers->keyBy('id');
+
+        foreach ($basketItems as $basketItem) {
+            $product = $basketItem->product;
+            if (!isset($product['sale_at']) && $offers->has($basketItem->offer_id)) {
+                $product['sale_at'] = $offers[$basketItem->offer_id]->sale_at;
+            }
+            $basketItem->product = $product;
+            $basketItem->save();
+        }
+    }
+}
