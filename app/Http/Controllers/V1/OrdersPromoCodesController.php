16125027d5d51bb14663280f8e8d700be376dd20
Kapitanov Andrey <andrey@kapdx.ru>
2020-05-06 12:18:38 +0300

    #59195 количество применений промокода

--- app/Http/Controllers/V1/OrdersPromoCodesController.php
+++ app/Http/Controllers/V1/OrdersPromoCodesController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order\Order;
 use App\Models\Order\OrderPromoCode;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\JsonResponse;
 
 
@@ -22,9 +24,24 @@ class OrdersPromoCodesController extends Controller
      */
     public function count(int $promoCodeId): JsonResponse
     {
+        $data = $this->validate(request(), [
+            'customer_id' => 'nullable|integer',
+        ]);
+
+        if (!isset($data['customer_id'])) {
+            $count = OrderPromoCode::query()->where('promo_code_id', $promoCodeId)->count();
+        } else {
+            $count = Order::query()
+                ->where('customer_id', $data['customer_id'])
+                ->whereHas('promoCodes',  function (Builder $query) use ($promoCodeId) {
+                    $query->where('promo_code_id', $promoCodeId);
+                })
+                ->count();
+        }
+
         return response()->json([
             'promo_code_id' => $promoCodeId,
-            'count'         => OrderPromoCode::query()->where('promo_code_id', $promoCodeId)->count()
+            'count'         => $count
         ]);
     }
 }
