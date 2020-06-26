b06d40cc00a3ccb002b3897bcc67e476acc58204
A Chernykh <nadycher@gmail.com>
2020-06-26 17:01:44 +0300

    #62122 Консольная команда получение url на онлайн оплату по id заказа

---
+++ app/Console/Commands/GetPayUrl.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Order\Order;
+use App\Services\PaymentService\PaymentService;
+use Illuminate\Console\Command;
+
+/**
+ * Команда для получения url оплаты
+ */
+class GetPayUrl extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'order:getpayurl {orderId}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Команда для получения url оплаты';
+
+    /**
+     * Execute the console command.
+     * @param PaymentService $paymentService
+     * @throws \Exception
+     */
+    public function handle(PaymentService $paymentService)
+    {
+        $orderId = $this->argument('orderId');
+        /** @var Order $order */
+        $order = Order::query()->where('id', $orderId)->with('payments')->first();
+        if (!$order) {
+            throw new \Exception("Заказ с id=$orderId не найден");
+        }
+
+        dump($paymentService->start($order->payments->first()->id, 'random'));
+    }
+}
