ce11fd84e5915dc1817f899796b97adaf770ea2c
Gruleva Olga <gruleva@greensight.ru>
2020-10-28 02:22:17 +0300

    #66704 float discount change value

---
+++ database/migrations/2020_10_28_011014_alter_order_discounts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterOrderDiscountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_discounts', function (Blueprint $table) {
+            $table->decimal('change', 18, 4)->default(0.0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_discounts', function (Blueprint $table) {
+            $table->integer('change')->change();
+        });
+    }
+}
