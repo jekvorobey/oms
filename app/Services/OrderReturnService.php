6061be3941c6cfb50959ad5a8fa5ec031f2e48f9
Vladimir Admaev <vladimir.admaev@ylab.io>
2021-09-01 11:27:42 +0400

    IBT-453 Add checking payment status of order

--- app/Services/OrderReturnService.php
+++ app/Services/OrderReturnService.php
@@ -7,6 +7,7 @@ use App\Models\Basket\BasketItem;
 use App\Models\Order\Order;
 use App\Models\Order\OrderReturn;
 use App\Models\Order\OrderReturnItem;
+use App\Models\Payment\PaymentStatus;
 use App\Services\Dto\In\OrderReturn\OrderReturnDto;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
@@ -19,75 +20,80 @@ class OrderReturnService
 {
     /**
      * Создать возврат по заказу
-     * @return array - [id, number]
+     * @return OrderReturn
      * @throws \Exception
      */
-    public function createOrderReturn(OrderReturnDto $orderReturnDto): array
+    public function createOrderReturn(OrderReturnDto $orderReturnDto): ?OrderReturn
     {
         $order = Order::find($orderReturnDto->order_id)->load('basket.items');
         if (!$order) {
             throw new \Exception("Order by id={$orderReturnDto->order_id} not found");
         }
 
-        return DB::transaction(function () use ($orderReturnDto, $order) {
-            $orderReturn = new OrderReturn();
-            $orderReturn->order_id = $order->id;
-            $orderReturn->customer_id = $order->customer_id;
-            $orderReturn->type = $order->type;
-            $orderReturn->number = OrderReturn::makeNumber($order->id);
-            $orderReturn->status = $orderReturnDto->status;
-            $orderReturn->save();
+        if ($order->payment_status !== PaymentStatus::PAID && $order->payment_status !== PaymentStatus::HOLD) {
 
-            $basketItemIds = $orderReturnDto->items->pluck('basket_item_id');
-            /** @var Collection|BasketItem[] $basketItems */
-            $basketItems = BasketItem::query()->whereIn('id', $basketItemIds)->get()->keyBy('id');
+            return DB::transaction(function () use ($orderReturnDto, $order) {
+                $orderReturn = new OrderReturn();
+                $orderReturn->order_id = $order->id;
+                $orderReturn->customer_id = $order->customer_id;
+                $orderReturn->type = $order->type;
+                $orderReturn->number = OrderReturn::makeNumber($order->id);
+                $orderReturn->status = $orderReturnDto->status;
+                $orderReturn->save();
 
-            foreach ($orderReturnDto->items as $item) {
-                $orderReturnItem = new OrderReturnItem();
-                $orderReturnItem->order_return_id = $orderReturn->id;
-                $orderReturnItem->basket_item_id = $item->basket_item_id;
-                if (!$basketItems->has($item->basket_item_id)) {
-                    throw new \Exception("BasketItem by id={$item->basket_item_id} not found");
-                }
-                $basketItem = $basketItems[$item->basket_item_id];
-                $orderReturnItem->offer_id = $basketItem->offer_id;
-                $orderReturnItem->referrer_id = $basketItem->referrer_id;
-                $orderReturnItem->bundle_id = $basketItem->bundle_id;
-                $orderReturnItem->type = $basketItem->type;
-                if ($order->type == Basket::TYPE_MASTER) {
-                    /**
-                     * Проверяем, что указаны id билетов для возврата
-                     */
-                    if (!$item->ticket_ids) {
-                        throw new \Exception("Returning ticket_ids for BasketItem with id={$item->basket_item_id} not specified");
+                $basketItemIds = $orderReturnDto->items->pluck('basket_item_id');
+                /** @var Collection|BasketItem[] $basketItems */
+                $basketItems = BasketItem::query()->whereIn('id', $basketItemIds)->get()->keyBy('id');
+
+                foreach ($orderReturnDto->items as $item) {
+                    $orderReturnItem = new OrderReturnItem();
+                    $orderReturnItem->order_return_id = $orderReturn->id;
+                    $orderReturnItem->basket_item_id = $item->basket_item_id;
+                    if (!$basketItems->has($item->basket_item_id)) {
+                        throw new \Exception("BasketItem by id={$item->basket_item_id} not found");
                     }
-                    /**
-                     * Проверяем, что id билетов для возврата указаны у BasketItem корзины заказа
-                     */
-                    if ($item->ticket_ids != array_intersect($basketItem->getTicketIds(), $item->ticket_ids)) {
-                        throw new \Exception("Returning ticket_ids for BasketItem with id={$item->basket_item_id} not contained at order");
+                    $basketItem = $basketItems[$item->basket_item_id];
+                    $orderReturnItem->offer_id = $basketItem->offer_id;
+                    $orderReturnItem->referrer_id = $basketItem->referrer_id;
+                    $orderReturnItem->bundle_id = $basketItem->bundle_id;
+                    $orderReturnItem->type = $basketItem->type;
+                    if ($order->type == Basket::TYPE_MASTER) {
+                        /**
+                         * Проверяем, что указаны id билетов для возврата
+                         */
+                        if (!$item->ticket_ids) {
+                            throw new \Exception("Returning ticket_ids for BasketItem with id={$item->basket_item_id} not specified");
+                        }
+                        /**
+                         * Проверяем, что id билетов для возврата указаны у BasketItem корзины заказа
+                         */
+                        if ($item->ticket_ids != array_intersect($basketItem->getTicketIds(), $item->ticket_ids)) {
+                            throw new \Exception("Returning ticket_ids for BasketItem with id={$item->basket_item_id} not contained at order");
+                        }
+                        $basketItem->setTicketIds($item->ticket_ids);
                     }
-                    $basketItem->setTicketIds($item->ticket_ids);
+                    $orderReturnItem->product = $basketItem->product;
+                    $orderReturnItem->name = $basketItem->name;
+                    $orderReturnItem->qty = $order->type == Basket::TYPE_PRODUCT ? $item->qty : count($item->ticket_ids);
+                    /**
+                     * Проверяем, что кол-во возвращаемого товара не больше, чем в корзине
+                     */
+                    if ($orderReturnItem->qty > $basketItem->qty) {
+                        throw new \Exception("Returning qty for BasketItem with id={$item->basket_item_id} more than at order");
+                    }
+                    $orderReturnItem->price = $basketItem->price / $basketItem->qty * $orderReturnItem->qty;
+                    $orderReturnItem->commission = 0; //todo Доделать расчет суммы удержанной комиссии
+                    $orderReturnItem->save();
                 }
-                $orderReturnItem->product = $basketItem->product;
-                $orderReturnItem->name = $basketItem->name;
-                $orderReturnItem->qty = $order->type == Basket::TYPE_PRODUCT ? $item->qty : count($item->ticket_ids);
-                /**
-                 * Проверяем, что кол-во возвращаемого товара не больше, чем в корзине
-                 */
-                if ($orderReturnItem->qty > $basketItem->qty) {
-                    throw new \Exception("Returning qty for BasketItem with id={$item->basket_item_id} more than at order");
-                }
-                $orderReturnItem->price = $basketItem->price / $basketItem->qty * $orderReturnItem->qty;
-                $orderReturnItem->commission = 0; //todo Доделать расчет суммы удержанной комиссии
-                $orderReturnItem->save();
-            }
 
-            $orderReturn->priceRecalc(false);
-            $orderReturn->commissionRecalc();
+                $orderReturn->priceRecalc(false);
+                $orderReturn->commissionRecalc();
 
-            return [$orderReturn->id, $orderReturn->number];
-        });
+                return $orderReturn;
+            });
+        }
+
+        return null;
     }
 
     public function updateOrderReturn(int $id, OrderReturnDto $orderReturnDto): void
