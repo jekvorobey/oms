e2d70c2b0e008a26ded84f9b431b7b0ba0020fe0
Vladimir Admaev <vladimir.admaev@ylab.io>
2021-09-03 14:17:26 +0400

    IBT-453 Fix migrations in tests

--- tests/Feature/OrderReturnReasonTest.php
+++ tests/Feature/OrderReturnReasonTest.php
@@ -5,28 +5,25 @@ namespace Tests\Feature;
 use App\Models\Basket\Basket;
 use App\Models\Order\OrderReturnReason;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 
 class OrderReturnReasonTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function createOrderReturnReasons(): Collection
-    {
-        $orderReturnReasons = factory(OrderReturnReason::class, 3)->create();
-
-        return collect($orderReturnReasons);
-    }
-
     public function testSuccessCancelOrderWithReturnReason(): void
     {
-        $orderReturnReasons = $this->createOrderReturnReasons();
-        $basket = factory(Basket::class)->create();
+        Event::fake();
+        $response = $this->putJson('api/v1/orders/return-reasons', [
+            'text' => 'First reason to return',
+        ]);
+        $response->assertStatus(204);
+
     }
 
     public function testFailedCancelOrderWithReturnReason(): void
     {
-
+        Event::fake();
     }
 }
