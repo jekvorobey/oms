e214334bea801462ec3b8fe5157f9ca00f271268
Максим Купров <maxim.kyprov@ylab.io>
2021-09-28 10:44:43 +0300

    IBT-453: some fixes

--- database/migrations/2020_07_06_111716_add_type_column_to_orders.php
+++ database/migrations/2020_07_06_111716_add_type_column_to_orders.php
@@ -14,7 +14,7 @@ class AddTypeColumnToOrders extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->integer('type')->unsigned()->after('basket_id')->default(0);
+            $table->integer('type')->unsigned()->after('basket_id');
         });
     }
 
