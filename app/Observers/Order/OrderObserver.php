0ed343badf7daaadb27e0373259cc28f70443773
Максим Купров <maxim.kyprov@ylab.io>
2021-12-06 12:00:29 +0300

    IBT-660: save quietly

--- app/Observers/Order/OrderObserver.php
+++ app/Observers/Order/OrderObserver.php
@@ -68,7 +68,8 @@ class OrderObserver
     public function created(Order $order)
     {
         $order->number = $order->id + 1000000;
-        $order->save();
+        Order::withoutEvents(fn() => $order->save());
+
         $order->basket->is_belongs_to_order = true;
         $order->basket->save();
 
