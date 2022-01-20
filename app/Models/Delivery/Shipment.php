6ea9f8968b694f69219415eecd56ff3491a8de53
Vladimir Admaev <vladimir.admaev@ylab.io>
2022-01-20 17:22:32 +0400

    IBT-535 Code style fixes

--- app/Models/Delivery/Shipment.php
+++ app/Models/Delivery/Shipment.php
@@ -13,7 +13,6 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Carbon;
 use Pim\Dto\Offer\OfferDto;
 use Pim\Dto\Product\ProductDto;
