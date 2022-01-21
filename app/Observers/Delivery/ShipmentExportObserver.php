247062ca5cf35f480042ea3a203909c60dfb9f25
Vladimir Admaev <vladimir.admaev@ylab.io>
2022-01-21 09:44:15 +0400

    IBT-535 Remove debug info

--- app/Observers/Delivery/ShipmentExportObserver.php
+++ app/Observers/Delivery/ShipmentExportObserver.php
@@ -4,7 +4,6 @@ namespace App\Observers\Delivery;
 
 use App\Models\Delivery\ShipmentExport;
 use Greensight\Message\Services\ServiceNotificationService\ServiceNotificationService;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use MerchantManagement\Services\MerchantService\MerchantService;
 use MerchantManagement\Services\OperatorService\OperatorService;
@@ -25,12 +24,6 @@ class ShipmentExportObserver
             $operators = $operatorService->operators($query);
             $merchantInfo = $merchantService->merchant($shipmentExport->shipment->merchant_id);
 
-            Log::debug(json_encode([
-                '$operators' => $operators,
-                'SHIPMENT_NUMBER' => $shipmentExport->shipment->number,
-                'MERCHANT' => $merchantInfo->legal_name,
-                'ERROR_MESSAGE' => $shipmentExport->err_message,
-            ]));
             foreach ($operators as $operator) {
                 $notificationService->send(
                     $operator->user_id,
