0d7a14ed8d9db5a694129e29b2a3a4655835c489
Vladimir Admaev <vladimir.admaev@ylab.io>
2022-01-21 12:14:09 +0400

    IBT-535 Fixes after code-review

--- app/Observers/Delivery/ShipmentExportObserver.php
+++ app/Observers/Delivery/ShipmentExportObserver.php
@@ -3,38 +3,31 @@
 namespace App\Observers\Delivery;
 
 use App\Models\Delivery\ShipmentExport;
+use Greensight\CommonMsa\Dto\RoleDto;
 use Greensight\Message\Services\ServiceNotificationService\ServiceNotificationService;
 use Illuminate\Support\Str;
 use MerchantManagement\Services\MerchantService\MerchantService;
-use MerchantManagement\Services\OperatorService\OperatorService;
 
 class ShipmentExportObserver
 {
     public function created(ShipmentExport $shipmentExport): void
     {
         if ($shipmentExport->err_code) {
-            /** @var OperatorService $operatorService */
-            $operatorService = app(OperatorService::class);
             /** @var ServiceNotificationService $notificationService */
             $notificationService = resolve(ServiceNotificationService::class);
             /** @var MerchantService $merchantService */
             $merchantService = resolve(MerchantService::class);
 
-            $query = $operatorService->newQuery()->setFilter('merchant_id', $shipmentExport->shipment->merchant_id);
-            $operators = $operatorService->operators($query);
             $merchantInfo = $merchantService->merchant($shipmentExport->shipment->merchant_id);
-
-            foreach ($operators as $operator) {
-                $notificationService->send(
-                    $operator->user_id,
-                    Str::slug('Ошибка экспорта отправления', '_'),
-                    [
-                        'SHIPMENT_NUMBER' => $shipmentExport->shipment->number,
-                        'MERCHANT' => $merchantInfo->legal_name,
-                        'ERROR_MESSAGE' => $shipmentExport->err_message,
-                    ]
-                );
-            }
+            $notificationService->sendByRole(
+                RoleDto::ROLE_LOGISTIC,
+                Str::slug('Ошибка экспорта отправления', '_'),
+                [
+                    'SHIPMENT_NUMBER' => $shipmentExport->shipment->number,
+                    'MERCHANT' => $merchantInfo->legal_name,
+                    'ERROR_MESSAGE' => $shipmentExport->err_message,
+                ]
+            );
         }
     }
 }
