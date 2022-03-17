1871e5fe8568216335ee08f46387c2ff90bc8e8f
Максим Купров <maxim.kyprov@ylab.io>
2022-03-17 11:33:42 +0300

    IBT-726: fix order cancelled notification to customer && order created to merchant

--- app/Observers/Delivery/ShipmentObserver.php
+++ app/Observers/Delivery/ShipmentObserver.php
@@ -464,43 +464,45 @@ class ShipmentObserver
         // }
 
         try {
+            /** @var ServiceNotificationService $serviceNotificationService */
             $serviceNotificationService = app(ServiceNotificationService::class);
+            /** @var OperatorService $operatorService */
             $operatorService = app(OperatorService::class);
+            /** @var UserService $userService */
+            $userService = app(UserService::class);
 
             $operators = $operatorService->operators(
                 (new RestQuery())
                     ->setFilter('merchant_id', '=', $shipment->merchant_id)
             );
 
+            $users = $userService->users(
+                $userService->newQuery()
+                    ->setFilter('id', $operators->pluck('user_id')->all())
+            )->keyBy('id');
+
             /** @var OperatorDto $operator */
-            foreach ($operators as $operator) {
-                $userService = app(UserService::class);
-
-                $user = $userService->users(
-                    $userService->newQuery()
-                        ->setFilter('id', $operator->user_id)
-                )->first();
-
-                $vars = [
-                    'QUANTITY_ORDERS' => 1,
-                    'LINK_ORDERS' => sprintf('%s/shipment/list/%d', config('mas.masHost'), $shipment->id),
-                    'CUSTOMER_NAME' => $user ? $user->first_name : '',
-                    'SUM_ORDERS' => (int) $shipment->cost,
-                    'GOODS_NAME' => $shipment->items->first()->basketItem->name,
-                    'QUANTITY_GOODS' => (int) $shipment->items->first()->basketItem->qty,
-                    'PRISE_GOODS' => (int) $shipment->items->first()->basketItem->price,
-                    'ALL_QUANTITY_GOODS' => (int) $shipment->items()->with('basketItem')->get()->sum('basketItem.qty'),
-                    'ALL_PRISE_GOODS' => (int) $shipment->cost,
-                ];
+            foreach ($operators as $i => $operator) {
+                $user = $users->get($operator->user_id);
+                $attributes = $this->generateCreatedNotificationAttributes($shipment, $user);
 
                 if ($user) {
+                    if ($i === 0) { // TODO: добавить проверку, что оператор является админов
+                        $serviceNotificationService->send(
+                            $operator->user_id,
+                            'klientoformlen_novyy_zakaz',
+                            $this->generateCreatedNotificationAttributes($shipment, $user)
+                        );
+                        continue;
+                    }
+
                     switch ($operator->communication_method) {
                         case OperatorCommunicationMethod::METHOD_PHONE:
                             $serviceNotificationService->sendDirect(
                                 'klientoformlen_novyy_zakaz',
                                 $user->phone,
                                 'sms',
-                                $vars
+                                $attributes
                             );
                             break;
                         case OperatorCommunicationMethod::METHOD_EMAIL:
@@ -508,17 +510,30 @@ class ShipmentObserver
                                 'klientoformlen_novyy_zakaz',
                                 $user->email,
                                 'email',
-                                $vars
+                                $attributes
                             );
                             break;
                     }
                 }
             }
-
-            $serviceNotificationService->send($operators[0]->user_id, 'klientoformlen_novyy_zakaz', $vars);
         } catch (\Throwable $e) {
             report($e);
         }
+    }
+
+    protected function generateCreatedNotificationAttributes(Shipment $shipment, ?UserDto $user): array
+    {
+        return [
+            'QUANTITY_ORDERS' => 1,
+            'LINK_ORDERS' => sprintf('%s/shipment/list/%d', config('mas.masHost'), $shipment->id),
+            'CUSTOMER_NAME' => $user ? $user->first_name : '',
+            'SUM_ORDERS' => (int) $shipment->cost,
+            'GOODS_NAME' => $shipment->items->first()->basketItem->name,
+            'QUANTITY_GOODS' => (int) $shipment->items->first()->basketItem->qty,
+            'PRISE_GOODS' => (int) $shipment->items->first()->basketItem->price,
+            'ALL_QUANTITY_GOODS' => (int) $shipment->items()->with('basketItem')->get()->sum('basketItem.qty'),
+            'ALL_PRISE_GOODS' => (int) $shipment->cost,
+        ];
     }
 
     protected function sendStatusNotification(Shipment $shipment)
