6ad3345e2285f2356cd43feb57cf0bfae2fbcb53
Vladimir Admaev <vladimir.admaev@ylab.io>
2022-04-05 16:16:11 +0400

    hotfix/v1.11.3-fix-order-receiver-data Fix getting receiver phone

--- app/Observers/Order/OrderObserver.php
+++ app/Observers/Order/OrderObserver.php
@@ -597,6 +597,8 @@ class OrderObserver
             ->join('<br>');
 
         [$receiverFullName, $receiverPhone] = [$order->deliveries->first()->receiver_name, $order->deliveries->first()->receiver_phone];
+        /* Форматы хранения телефона в доставке и у пользователя, поэтому приводим к единому виду (как у пользователя) */
+        $receiverPhone = str_replace(['(', ')', '-', ' '], '', $receiverPhone);
 
         if (empty($deliveryAddress)) {
             $deliveryAddress = 'ПВЗ';
