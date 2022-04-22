46e265f2ea56ab756fdaa4d49d52f24504966b87
Evgeniy Mokrov <e.mokrov@ylab.io>
2022-04-22 11:06:08 +0300

    IBT-724: common fixes

--- app/Observers/Basket/BasketItemObserver.php
+++ app/Observers/Basket/BasketItemObserver.php
@@ -135,7 +135,7 @@ class BasketItemObserver
 
     private function returnBonuses(BasketItem $basketItem): void
     {
-        if ($basketItem->bonus_spent != $basketItem->getOriginal('bonus_spent')) {
+        if ($basketItem->bonus_spent != $basketItem->getOriginal('bonus_spent') && $basketItem->qty_canceled) {
             $spent = $basketItem->getOriginal('bonus_spent') - $basketItem->bonus_spent;
             $order = $basketItem->basket->order;
             /** @var CustomerService $customerService */
