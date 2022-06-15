d546d2cfd9d18ec080836059159514398c53e2bb
Максим Купров <maxim.kyprov@ylab.io>
2022-06-15 12:04:49 +0300

    BX-6199: fix cs

--- database/migrations/2022_05_31_105004_add_settings_to_payment_methods_table.php
+++ database/migrations/2022_05_31_105004_add_settings_to_payment_methods_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\Artisan;
 
 class AddSettingsToPaymentMethodsTable extends Migration
 {
